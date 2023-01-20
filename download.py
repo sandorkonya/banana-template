933b13f4d84599e68f44ccffb9638820c8cfdd93
dr. Konya Sandor <sandorkonya@users.noreply.github.com>
2023-01-20 12:53:14 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,13 +1,13 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
 
-# In this example: A Huggingface BERT model
+# In this example: a timm model
 
-from transformers import pipeline
+import timm
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    model = timm.create_model("hf_hub:timm/mobilenetv3_large_100.ra_in1k", pretrained=True)
 
 if __name__ == "__main__":
     download_model()
